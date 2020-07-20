6e710d514e8eba794f15f165bbc6864ca87ab515
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-07-20 15:04:44 -0700

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,21 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    first = 0
+    last = len(arr) - 1
 
+    found = False
+
+    while first <= last and not found:
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            return middle
+
+        else:
+            if target < arr[middle]:
+                last = middle - 1
+            if target > arr[middle]:
+                first = middle + 1
 
     return -1  # not found
